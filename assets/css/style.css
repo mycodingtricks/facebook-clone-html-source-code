df329b613de24a93baa0f74a9f2fdf514fc0dc56
Shubham <kshubham52@gmail.com>
2017-04-23 23:35:56 +0530

    Create style.css

---
+++ assets/css/style.css
@@ -0,0 +1,224 @@
+*{
+    margin:0px;
+    box-sizing:border-box;
+    transition: 0.2s all ease-in-out;
+    -webkit-transition: 0.2s all ease-in-out;
+    -o-transition: 0.2s all ease-in-out;
+    -moz-transition: 0.2s all ease-in-out;
+}
+:after, :before {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+}
+body{font-family: Arial, Verdana, Sans-serif;font-size:14px;padding-top:50px;}
+.container-fluid {
+    padding-right: 15px;
+    padding-left: 15px;
+    margin-right: auto;
+    margin-left: auto;
+    max-width:1200px;
+}
+.row{
+    margin:0px -15px;
+}
+.row:after, .row:before {
+    display: table;
+    content: " ";
+}
+.row:after {
+    clear: both;
+}
+a{
+    text-decoration:none;
+    color:#2d9a40;
+}
+.pull-right{
+    float:right;
+}
+.pull-left{
+    float:left;
+}
+.img-responsive{
+    max-width:100%;
+    height:auto;
+}
+p{margin-bottom:5px;}
+h1,.h1{
+    font-size:35px;
+    line-height:40px;
+}
+h2,.h2{
+    font-size:30px;
+    line-height:350px;
+}
+h3,.h3{
+    font-size:25px;
+    line-height:30px;
+}
+h4,.h4{
+    font-size:20px;
+    line-height:25px;
+}
+h5,.h5{
+    font-size:15px;
+    line-height:20px;
+}
+h6,.h6{
+    font-size:10px;
+    line-height:15px;
+}
+/*Columns*/
+*[class*=col-]{float:left;padding:0px 15px;width:100%;min-height:1px;}
+@media (min-width: 768px) {
+  .col-sm-1, .col-sm-2, .col-sm-3, .col-sm-4, .col-sm-5, .col-sm-6, .col-sm-7, .col-sm-8, .col-sm-9, .col-sm-10, .col-sm-11, .col-sm-12 {
+    float: left;
+  }
+  .col-sm-12 {
+    width: 100%;
+  }
+  .col-sm-11 {
+    width: 91.66666667%;
+  }
+  .col-sm-10 {
+    width: 83.33333333%;
+  }
+  .col-sm-9 {
+    width: 75%;
+  }
+  .col-sm-8 {
+    width: 66.66666667%;
+  }
+  .col-sm-7 {
+    width: 58.33333333%;
+  }
+  .col-sm-6 {
+    width: 50%;
+  }
+  .col-sm-5 {
+    width: 41.66666667%;
+  }
+  .col-sm-4 {
+    width: 33.33333333%;
+  }
+  .col-sm-3 {
+    width: 25%;
+  }
+  .col-sm-2 {
+    width: 16.66666667%;
+  }
+  .col-sm-1 {
+    width: 8.33333333%;
+  }
+}
+
+/* Header */
+.header {
+    width: 100%;
+    background: #2d9a40;
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    z-index:999;
+    box-shadow: 0px 5px 5px 0px rgba(0, 0, 0, 0.45);
+}
+.header .logo h1{
+    line-height:48px;
+}
+.header .logo a{
+    color:#fff;
+}
+.header *[class*=col-]{
+    padding:5px 10px;
+    color:#fff;
+}
+/*.header .inline-form{
+    padding:5px 10px;
+}*/
+/* Main */
+.main{
+    min-height:100%;
+    padding: 30px 0px;
+    background: url(../imgs/wall.jpg) fixed center;
+}
+/* Backgrounds */
+.gradient{
+    background: linear-gradient(white, #D3D8E8);
+}
+
+/*Buttons*/
+.btn {
+    background: rgba(0,0,0,0.4);
+    color: #fff;
+    border: 1px solid rgba(0,0,0,0.2);
+    box-shadow: 2px 2px 0px 0px rgba(0, 0, 0, 0.2);
+    cursor: pointer;
+    outline: none;
+    padding: 5px 10px;
+    font-size: 14px;
+    /* line-height: 28px; */
+    border-radius: 2px;
+    font-weight: bold;
+    text-align: center;
+}
+.btn:active{
+    box-shadow:none;
+}
+.btn-primary {
+    background: #278537;
+}
+.btn-primary:hover {
+    background: #1f6b2c;
+}
+.btn-success {
+    background: #4CAF50;
+}
+.btn-success:hover{
+    background: #3d8c40;
+}
+/*Text*/
+.text-primary{
+    color: #0e385f;
+}
+.text-center{
+    text-align:center;
+}
+.text-mute{color:#777;}
+/* Form */
+form{
+    padding:10px 0px;
+}
+input,select,textarea,button{
+    padding:5px 10px;
+    border:1px solid rgba(0,0,0,0.2);
+    border-radius:2px;
+    outline: none;
+}
+input:focus,select:focus,textarea:focus,button:focus{
+    border:1px solid rgba(0,0,0,0.5);
+    outline:none;
+}
+.input-lg{
+    font-size:20px;
+    padding:10px 20px;
+}
+.inline-form input{
+    display:inline-block;
+}
+.form-group{
+    display:inline-block;
+    width:100%;
+    margin-bottom:10px;
+}
+/*Footer*/
+.footer {
+    padding: 20px 15px;
+    box-shadow: 0px -6px 5px 0px rgba(0,0,0,0.5);
+    background: rgba(251, 249, 249, 0.45);
+}
+/* Responsive */
+@media (max-width:768px){
+    body{padding-top:0px}
+    .header{position:relative;}
+    .header .logo{text-align:center}
+}
